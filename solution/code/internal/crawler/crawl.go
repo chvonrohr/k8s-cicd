b4d40a4b8447fa88bc276aaca5b89e0ff909e686
Carlo Field <cfi@bluesky-it.ch>
2020-07-01 12:27:53 +0200

    add crawling code and parse anchor tags

--- solution/code/internal/crawler/crawl.go
+++ solution/code/internal/crawler/crawl.go
@@ -17,5 +17,38 @@ func Crawl(url string) ([]string, error) {
 	if r.Header.Get("content-type") != "text/html" {
 		return nil, errors.New("invalid content type")
 	}
-	z := html.NewTokenizer(r.Body)
+	node, err := html.Parse(r.Body)
+	if err != nil {
+		return nil, err
+	}
+	anchors := crawlNode(node)
+	urls := make([]string, 0)
+	for _, anchor := range anchors {
+		url := getHref(anchor)
+		if url != "" {
+			urls = append(urls, url)
+		}
+	}
+	return urls, nil
+}
+
+func getHref(anchor *html.Node) string {
+	for _, attribute := range anchor.Attr {
+		if attribute.Key == "href" {
+			return attribute.Val
+		}
+	}
+	return ""
+}
+
+func crawlNode(node *html.Node) []*html.Node {
+	anchors := make([]*html.Node, 0)
+	if node.Type == html.ElementNode && node.Data == "a" {
+		anchors = append(anchors, node)
+	}
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		as := crawlNode(child)
+		anchors = append(anchors, as...)
+	}
+	return anchors
 }
