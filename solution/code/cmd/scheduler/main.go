56516a2f7933a094f4e79c09807b9d46bc349d75
cfi <carlo@swiss.dev>
2020-08-04 16:40:41 +0200

    scheduler code comments

--- solution/code/cmd/scheduler/main.go
+++ solution/code/cmd/scheduler/main.go
@@ -6,13 +6,17 @@ import (
 )
 
 func main() {
+	// default scheduler endpoint
 	url := "http://letsboot-backend/schedule"
 
+	// allow overriding of endpoint via argument
 	if len(os.Args) > 1 {
 		url = os.Args[1]
 	}
 
+	// run a post against the endpoint
 	r, err := http.Post(url, "", nil)
+	// if anything goes wrong, exit with a failed status => kubernetes will recognise the invalid exit code
 	if err != nil {
 		panic(err)
 	}
