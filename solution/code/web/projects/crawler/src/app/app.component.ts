139425c4c8530abed19bcd204971cb5e86d5a6ae
Jonas Felix <jonas.felix@felixideas.ch>
2020-08-07 16:28:46 +0200

    backend detection

--- solution/code/web/projects/crawler/src/app/app.component.ts
+++ solution/code/web/projects/crawler/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { timeout } from 'rxjs/operators';
 
 @Component({
   selector: 'crl-root',
@@ -10,15 +11,44 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit {
   title = 'crawler';
   sites: Observable<any>;
+  url: string;
+  tryUrls = ['/api', 'http://localhost:8080', 'http://localhost/api'];
+  error: string;
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
-    this.sites = this.http.get('http://localhost:8080/sites');
+    this.tryUrl(0);
+  }
+
+  tryUrl(index) {
+    const tryUrl = this.tryUrls[index];
+    if (tryUrl !== undefined) {
+      this.http.get(tryUrl, { responseType: 'text' })
+        .pipe(timeout(100))
+        .subscribe(
+          response => {
+            this.url = tryUrl;
+            this.getSites();
+          },
+          error => {
+            this.tryUrl(index + 1);
+          }
+        )
+    } else {
+      this.error = 'no running backend found'
+    }
+  }
+
+  getSites() {
+    this.sites = this.http.get(this.url + '/sites');
   }
 
   addSite(site) {
-    this.sites = this.http.post('http://localhost:8080/sites', { url: site, interval: 100 });
+    this.http.post(this.url + '/sites', { url: site, interval: 100 }).subscribe(
+      response => this.getSites(),
+      error => this.error = error.message
+    );
   }
 }
