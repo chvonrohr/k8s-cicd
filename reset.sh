97f70e9351f2ba0507cf410ec2471e2be6708c82
Jonas Felix <jonas.felix@felixideas.ch>
2020-08-29 17:24:24 +0200

    project-start reset script

--- reset.sh
+++ reset.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# resets:
-# - docker
-# - kind
-# - gke
-
+read -p "Are you sure? " -n 1 -r
+echo -e "\n"
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+    rm -i project-start/build/ci/*
+    rm -i project-start/build/package/*.Dockerfile
+    rm -i project-start/deployments/*.yaml
+    rm -i project-start/deployments/*/*.yaml
+then
+    exit 1
+fi
+echo -e "\n"
