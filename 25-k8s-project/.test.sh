d6c1c3f71988a298c7e174b5ad8f7d83d93dc7f9
Jonas Felix <jonas.felix@felixideas.ch>
2020-09-09 22:48:17 +0200

    kind test fix

--- 25-k8s-project/.test.sh
+++ 25-k8s-project/.test.sh
@@ -42,6 +42,8 @@ for deployment in queue database backend frontend crawler; do \
     kubectl wait deployments/$deployment --for condition=available ||errout "deployment $deployment not available"
 done
 
+sleep 20
+
 curl http://localhost/api/ ||errout "no backend"
 [ "$(curl http://localhost/api/)" == "backend works" ] || ||errout "backend doesn't indicate to work"
 curl http://localhost ||errout "no frontend"
